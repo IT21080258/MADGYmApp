0fbb1b0b6ba47da267780f2b0b703ddc7662fec1
IT21080258 <it21080258@my.sliit.lk>
2022-10-09 08:19:13 +0530

    Login Created

--- app/src/main/java/com/example/olexogymmad/login.java
+++ app/src/main/java/com/example/olexogymmad/login.java
@@ -2,15 +2,18 @@ package com.example.olexogymmad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class login extends AppCompatActivity {
 
     EditText username, password;
     Button loginBtn;
+    DBHelper DB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +23,27 @@ public class login extends AppCompatActivity {
         username = (EditText) findViewById(R.id.uNameLog);
         password = (EditText) findViewById(R.id.uPasswordLog);
         loginBtn = (Button) findViewById(R.id.loginBtn);
+        DB = new DBHelper(this);
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
 
+                if(user.equals("")||pass.equals(""))
+                    Toast.makeText(login.this,"Please enter all details",Toast.LENGTH_SHORT).show();
+                else{
+                    Boolean userPass = DB.checkUserDetails(user,pass);
+                    if (userPass == true){
+                        Toast.makeText(login.this,"Login Successful",Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                        startActivity(intent);
+                    }
+                    else{
+                        Toast.makeText(login.this,"Username or Password doesn't match",Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
         });
     }
