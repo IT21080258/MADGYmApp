ea796261167bb75e5774b2039db953f8294ab9aa
IT21080258 <it21080258@my.sliit.lk>
2022-10-09 08:28:19 +0530

    Manifest fixed so that app will load into login activity

---
+++ app/src/main/java/com/example/olexogymmad/trainers.java
@@ -0,0 +1,14 @@
+package com.example.olexogymmad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class trainers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trainers);
+    }
+}
